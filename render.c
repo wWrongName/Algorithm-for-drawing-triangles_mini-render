ba54b99ff9c1181f48b1d9a6c03383fdb4f29921
Simple <44000608+wWrongName@users.noreply.github.com>
2019-05-04 23:45:42 +0300

    Update render.c

--- render.c
+++ render.c
@@ -4,7 +4,7 @@
 #include "render.h"
 #define SWAP(x,y) x=x^y; y=y^x; x=x^y; 
 
-RGB color(unsigned char R, unsigned char G, unsigned char B) {
+RGB set_color(unsigned char R, unsigned char G, unsigned char B) {
 	RGB clr_of_pix;
 	clr_of_pix.blue  = B;
 	clr_of_pix.green = G;
@@ -12,26 +12,35 @@ RGB color(unsigned char R, unsigned char G, unsigned char B) {
 	return clr_of_pix;
 }
 
-void line(RGB **image, RGB color, int x0, int y0, int x1, int y1) {
-	int delta_x = x1 - x0;
-	int delta_y = y1 - y0;
+C_VEC set_vector(int x0, int y0, int x1, int y1) {
+	C_VEC out_vector;
+	out_vector.x0 = x0;
+	out_vector.y0 = y0;
+	out_vector.x1 = x1;
+	out_vector.y1 = y1;
+	return out_vector;
+}
+
+void line(RGB **image, RGB color, C_VEC *l_vector) {
+	int delta_x = l_vector->x1 - l_vector->x0;
+	int delta_y = l_vector->y1 - l_vector->y0;
 	char rot = 0;
 	int y;
 
 	if (abs(delta_x) < abs(delta_y)){
-		SWAP(x0, y0);
-		SWAP(x1, y1);
+		SWAP(l_vector->x0, l_vector->y0);
+		SWAP(l_vector->x1, l_vector->y1);
 		rot = 1;
 	}
 
-	if (x0 > x1) {
-		SWAP(x0, x1);
-		SWAP(y0, y1);
+	if (l_vector->x0 > l_vector->x1) {
+		SWAP(l_vector->x0, l_vector->x1);
+		SWAP(l_vector->y0, l_vector->y1);
 	}
 
-	for (int x = x0; x < x1; x++) {
+	for (int x = l_vector->x0; x < l_vector->x1; x++) {
 		float k = (delta_y) / (float)(delta_x);
-		y = k * (x - x0) + y0;
+		y = k * (x - l_vector->x0) + l_vector->y0;
 		if (rot)
 			image[y][x] = color;
 		else 
