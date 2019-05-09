b5395af4623ba64a428c63097b74b6f0f577e2f4
Simple <44000608+wWrongName@users.noreply.github.com>
2019-05-09 23:06:18 +0300

    Update render.h

--- render.h
+++ render.h
@@ -52,9 +52,12 @@ typedef struct TRGL_COORDS{
 
 COORD set_coord(int x, int y);
 C_VEC set_vector(COORD one, COORD two);
+TRIANGLE *sort_coords(TRIANGLE *tgl);
+void draw_triangle(RGB **image, RGB color, TRIANGLE *tgl);
 TRIANGLE set_triangle(COORD one, COORD two, COORD three);
 RGB set_color(unsigned char R, unsigned char G, unsigned char B);  
-void draw_triangle(RGB **image, RGB color, TRIANGLE tgl);
+void draw_outline_of_triangle(RGB **image, RGB color, TRIANGLE *tgl);
+void draw_plain_triangle(RGB **image, RGB color, TRIANGLE *tgl);
 void draw_line(RGB **image, RGB color, C_VEC l_vector);  
 
 #endif
